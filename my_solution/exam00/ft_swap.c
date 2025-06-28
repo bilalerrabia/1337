0dc29b535c4c14bf762e05b06b26196dbd2133d6
bilal errabia <115508904+bilalerrabia@users.noreply.github.com>
2025-06-28 04:48:55 +0100

    Delete my_solution directory

--- my_solution/exam00/ft_swap.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void ft_swap(int *a,int *b){
-
-	int tmp=*a;
-	*a=*b;
-	*b=tmp;
-}
--- my_solution/exam00/maff_revalpha.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int main(){
-
- 	write(1,"zYxWvUtSrQpOnMlKjIhGfEdCbA/n",27);
-
-
-}
